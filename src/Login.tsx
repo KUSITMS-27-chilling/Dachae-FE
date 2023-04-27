e28163a767319366b80d97bd5bff76b150037abb
yeonsu <kimys9295@naver.com>
2023-04-27 20:19:40 +0900

    fix : 로그인수정(axios연결)

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect,useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 //const navigate = useNavigate();
 
 
@@ -8,21 +9,45 @@ function Login() {
   const navigate = useNavigate();
   const navigateJoin =()=>{
     navigate("/join");
-  }
+  } //join으로 아동
+
+  const [inputId, setInputId] = useState('')
+  const [inputPw, setInputPw] = useState('')
+
+  const handleInputId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputId(e.target!.value)
+}
+
+const handleInputPw = (e:React.ChangeEvent<HTMLInputElement> ) => {
+    setInputPw(e.target.value)
+}
+
+// login 버튼 클릭 이벤트
+const onClickLogin = () => {
+  axios.post(`${import.meta.env.VITE_APP_HOST}/user/login`, null, {
+    params: {
+    id: inputId,
+    password: inputPw
+    }
+})
+.then(res => console.log(res))
+.catch()
+}
+
     
   return (
       <div>
             <h2>Login</h2>
             <div>
                 <label htmlFor='input_id'>ID : </label>
-                <input type='text' name='input_id' />
+                <input type='text' name='input_id' value={inputId} onChange={handleInputId} />
             </div>
             <div>
                 <label htmlFor='input_pw'>PW : </label>
-                <input type='password' name='input_pw' />
+                <input type='password' name='input_pw' value={inputPw} onChange={handleInputPw} />
             </div>
             <div>
-                <button type='button'>Login</button>
+                <button type='button' onClick={onClickLogin}>Login</button>
             </div><div>
                 <button type='button' onClick={navigateJoin}>Join</button>
             </div>
