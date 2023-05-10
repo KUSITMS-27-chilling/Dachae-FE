e940b6a15945cc0d3f607baa1ccea3dc5a890cce
leehyewon0531 <ifpffk@naver.com>
2023-05-10 14:32:30 +0900

    feat: 같이듣기 api 연결

--- src/components/DetailContent/DetailContent.tsx
+++ src/components/DetailContent/DetailContent.tsx
@@ -6,10 +6,15 @@ import { detailTabKind } from "../../recoil/detail";
 import { useRecoilValue } from "recoil";
 import temp_program from "../../assets/temp_program.jpg";
 import { detailOrCommu } from "../../types/review";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { TogetherData } from "../../types/together";
 
 function DetailContent() {
   const detailTab = useRecoilValue(detailTabKind);
   const detail: detailOrCommu = 'detail';
+  const [togetherArr, setTogetherArr] = useState<TogetherData[]>([]);
 
   const reviewData = {
     userNick: '다채 고수',
@@ -32,12 +37,58 @@ function DetailContent() {
     detailOrCommu: detail
   }
 
+  const { region } = useParams();
+
+  const getListenTogether = async () => {
+    await axios.get(`${import.meta.env.VITE_APP_HOST}/listen/page/${region}`)
+      .then((res) => {
+        const response = res.data.data.listenTogethers;
+        for(let key in response) {
+          const object: TogetherData = {
+            nickname: response[key].nickName,
+            time: '18시간 전',
+            recruiting: response[key].recruiting,
+            title: response[key].title,
+            favFields: response[key].favFields,
+            goalNum: response[key].goalNum,
+            currentNum: response[key].currentNum,
+            tags: response[key].tags,
+            programName: response[key].programName
+          }
+
+          setTogetherArr(togetherArr => [object, ...togetherArr]);
+        }
+      })
+      .catch((err) => console.log(err));
+    return;
+  }
+
+  const getReview = () => {
+
+  }
+
+  useEffect(() => {
+    setTogetherArr([]);
+    if(detailTab == 'together'){
+      getListenTogether();
+      console.log(togetherArr);
+      return;
+    }
+    if(detailTab == 'review'){
+      getReview();
+      return;
+    }
+  }, [detailTab]);
+
   return(
     <DetailContentContainer>
       <DetailTabBar />
       {
         detailTab == 'together' &&
-        <Together />
+        (togetherArr.map((el, idx) => {
+          return <Together key={idx} prop={el} />
+        }))
+        // <Together />
       }
       {
         detailTab == 'review' &&
--- src/components/Together/Together.tsx
+++ src/components/Together/Together.tsx
@@ -11,27 +11,28 @@ import {
 import { useEffect, useState } from "react";
 import recruiting_img from '../../assets/recruiting.png';
 import recruited_img from '../../assets/recruited.png';
+import { TogetherProp } from "../../types/together";
 
-function Together () {
+function Together ({ prop }: TogetherProp) {
   const [favStr, setFavStr] = useState<string>('');
 
   // 추후 api 호출 반영을 위한 동적 데이터
-  const props = {
-    nickname: '다채 고수',
-    time: '18시간 전',
-    recruiting: true,
-    title: '성복구 레진아트 강좌 같이 들을 분 계실까요?^^',
-    favFields: [
-      "문화여가", "자기계발"
-    ],
-    goalNum: 5,
-    hits: 2,
-    tags: [
-      "은평구",
-      "봉산아래공동체주택동네배움터"
-    ],
-    programName: "초보를 위한 레진아트 수업"
-  }
+  // const props = {
+  //   nickname: '다채 고수',
+  //   time: '18시간 전',
+  //   recruiting: true,
+  //   title: '성복구 레진아트 강좌 같이 들을 분 계실까요?^^',
+  //   favFields: [
+  //     "문화여가", "자기계발"
+  //   ],
+  //   goalNum: 5,
+  //   currentNum: 2,
+  //   tags: [
+  //     "은평구",
+  //     "봉산아래공동체주택동네배움터"
+  //   ],
+  //   programName: "초보를 위한 레진아트 수업"
+  // }
 
   const favToStr = (favFields: Array<string>):void => {
     let tempStr = '';
@@ -52,7 +53,7 @@ function Together () {
   }
 
   useEffect(() => {
-    favToStr(props.favFields);
+    favToStr(prop.favFields);
   }, [])
 
   return(
@@ -60,26 +61,26 @@ function Together () {
       <ReviewProfile topProp={true}>
         <div id="review__profile-photo"></div>
         <div>
-          <div id="review__profile-nickname">{props.nickname}</div>
-          <div id="review__profile-time">{props.time}</div>
+          <div id="review__profile-nickname">{prop.nickname}</div>
+          <div id="review__profile-time">{prop.time}</div>
         </div>
       </ReviewProfile>
-      <TogetherTitle recruiting={props.recruiting}>
-        <div id="together__recruiting">{props.recruiting ? "모집중" : "모집완료"}</div>
-        <div id="together__title">{props.title}</div>
+      <TogetherTitle recruiting={prop.recruiting}>
+        <div id="together__recruiting">{prop.recruiting ? "모집중" : "모집완료"}</div>
+        <div id="together__title">{prop.title}</div>
       </TogetherTitle>
       <TogetherInfo>
         <div id="together__fav-fields">지원자 활동 분야: {favStr}</div>
-        <div id="together__goal-num">희망 인원: {props.goalNum}명</div>
+        <div id="together__goal-num">희망 인원: {prop.goalNum}명</div>
       </TogetherInfo>
       <TogetherFooter>
         <ReviewPlace topProp={true} >
-            <div id="review__place-region">&#35;{props.tags[0]}</div>
-            <div id="review__place-detail">&#35;{props.tags[1]}</div>
+            <div id="review__place-region">&#35;{prop.tags[0]}</div>
+            <div id="review__place-detail">&#35;{prop.tags[1]}</div>
         </ReviewPlace>
-        <div id="together__program-name">{props.programName}</div>
-        <img id="together__recruit-img" src={props.recruiting ? recruiting_img : recruited_img} alt="recruit-img" />
-        <div id="together__hit-and-goal">{props.hits}/{props.goalNum}</div>
+        <div id="together__program-name">{prop.programName}</div>
+        <img id="together__recruit-img" src={prop.recruiting ? recruiting_img : recruited_img} alt="recruit-img" />
+        <div id="together__hit-and-goal">{prop.currentNum}/{prop.goalNum}</div>
       </TogetherFooter>
     </TogetherContainer>
   )
