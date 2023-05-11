9b1395c29f66354b064a48246c1b6ca5b608ba86
leehyewon0531 <ifpffk@naver.com>
2023-05-11 12:33:49 +0900

    comment: DetailContent 주석 정리

--- src/components/DetailContent/DetailContent.tsx
+++ src/components/DetailContent/DetailContent.tsx
@@ -4,7 +4,6 @@ import Review from "../Review";
 import { DetailContentContainer } from "./DetailContent.styled";
 import { detailTabKind } from "../../recoil/detail";
 import { useRecoilValue } from "recoil";
-import temp_program from "../../assets/temp_program.jpg";
 import { detailOrCommu, ReviewData } from "../../types/review";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -13,30 +12,8 @@ import { TogetherData } from "../../types/together";
 
 function DetailContent() {
   const detailTab = useRecoilValue(detailTabKind);
-  const detail: detailOrCommu = 'detail';
   const [togetherArr, setTogetherArr] = useState<TogetherData[]>([]);
   const [reviewArr, setReviewArr] = useState<ReviewData[]>([]);
-
-  const reviewData = {
-    userNick: '다채 고수',
-    time: '18시간 전',
-    title: '[1주차 후기] 강의 들은 후기입니다 (제목자리)',
-    lecture: '신박한 정리를 위한 미니멀리즘(강의명)',
-    content: `가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다
-    가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다
-    가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다
-    가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다
-    가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다 가 나 다
-    가 나 다 가 나 다 가 나 다 가 나`,
-    region: '은평구',
-    detailPlace: '봉산아래공동체주택동네배움터',
-    reviewImg: [
-      temp_program,
-      temp_program,
-      temp_program
-    ],
-    detailOrCommu: detail
-  }
 
   const { region } = useParams();
 
